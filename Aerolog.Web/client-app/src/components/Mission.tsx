59ac6d167f9803d8c4c8ed5d6e753a0a49fedb82
Daric Teske <dteske25@users.noreply.github.com>
2021-04-11 14:54:52 -0500

    remove timeline component

--- Aerolog.Web/client-app/src/components/Mission.tsx
+++ Aerolog.Web/client-app/src/components/Mission.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Timeline from './Timeline';
 
 const Mission = () => {
   return (
@@ -8,25 +7,7 @@ const Mission = () => {
         <div style={{ backgroundColor: 'red', flex: '4 0 auto' }}>slide show</div>
         <div style={{ backgroundColor: 'blue', flex: '1 0 auto' }}>chat area</div>
       </div>
-      <div style={{ backgroundColor: 'green', flex: '1 0 0' }}>
-        <Timeline
-          events={[
-            { text: 'Launch', timestamp: '1969-07-16 08:32:00 AM' },
-            { text: 'Translunar Injection', timestamp: '1969-07-16 11:16:16 AM' },
-            { text: 'CSM-LM docking', timestamp: '1969-07-16 11:56:03 AM' },
-            { text: 'Lunar orbit insertion', timestamp: '1969-07-19 12:21:50 AM' },
-            { text: 'CSM-LM separation', timestamp: '1969-07-20 01:11:53 PM' },
-            { text: 'Lunar landing', timestamp: '1969-07-20 03:17:40 PM' },
-            { text: 'Begin EVA', timestamp: '1969-07-20 09:39:33 PM' },
-            { text: 'First step on surface', timestamp: '1969-07-20 09:56:15 PM' },
-            { text: 'Lunar liftoff', timestamp: '1969-07-21 12:54:01 PM' },
-            { text: 'LM-CSM docking', timestamp: '1969-07-21 04:34:00 PM' },
-            { text: 'Transearth injection', timestamp: '1969-07-21 11:54:42 PM' },
-            { text: 'Splashdown', timestamp: '1969-07-24 11:50:35 AM' },
-          ]}
-          value={new Date()}
-        />
-      </div>
+      <div style={{ backgroundColor: 'green', flex: '1 0 0' }}></div>
     </div>
   );
 };
